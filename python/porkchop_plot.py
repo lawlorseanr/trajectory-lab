f2cef7464fdc0a2c767487e7ca568991a472ae06
Sean Lawlor <blue.dusk0520@fastmail.com>
2015-08-11 14:06:08 -0800

    archive 2018

--- python/porkchop_plot.py
+++ python/porkchop_plot.py
@@ -1,3 +1,5 @@
 """Toy porkchop grid — fictional Earth-Mars transfer."""
 def grid(c3_values, tof_days):
     return {(c3, tof): c3 * 0.01 * tof for c3 in c3_values for tof in tof_days}
+
+# note update 33
